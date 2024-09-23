a67240b25812dd3b9e2aaf0c430e24087e0fc03f
sahilsoni20 <ssahil3120@gmail.com>
2024-09-23 23:45:25 +0530

    intial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,9 @@
-import { Toaster } from 'react-hot-toast'
-import { ImageDownloader } from './components/ImageDownloader/ImageDownloader'
-import { ImageUploader } from './components/ImageUploader/ImageUploader'
 import './index.css'
 
 function App() {
 
   return (
-    <div className='main-container'>
-      <Toaster position={'top-center'} reverseOrder={false}/>
-      <ImageUploader/>
-      <ImageDownloader/>
-    </div>
+    <></>
   )
 }
 
