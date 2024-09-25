031049250581c666005ed45e22bb00b2adc37451
sahilsoni20 <ssahil3120@gmail.com>
2024-09-25 17:09:08 +0530

    feat: hook to save user

--- src/hooks/useUserStore.ts
+++ src/hooks/useUserStore.ts
@@ -1,12 +1,20 @@
-import { User } from "firebase/auth"
+import { User } from "firebase/auth";
 import { create } from "zustand";
+import { onAuthStateChanged } from "firebase/auth";
+import { firebaseAuth } from "../firebase/firebaseConfig"; // Your Firebase config
 
 type StoreType = {
-    currentUser: undefined | null | User;
-    setCurrentUser: (user: User | null) => void;
-}
+  currentUser: User | null;
+  setCurrentUser: (user: User | null) => void;
+};
 
 export const useUserStore = create<StoreType>((set) => ({
-    currentUser: undefined,
-    setCurrentUser: (user) => set({currentUser: user})
-}))
+  currentUser: null,
+  setCurrentUser: (user) => set({ currentUser: user }),
+}));
+
+// Listen for changes in authentication state when app starts
+onAuthStateChanged(firebaseAuth, (user) => {
+  const setCurrentUser = useUserStore.getState().setCurrentUser;
+  setCurrentUser(user); // Set the user in Zustand store
+});
