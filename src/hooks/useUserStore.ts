6dbd8cfc7c5639b5088d4bc5b492e7bcf0eb3122
sahilsoni20 <ssahil3120@gmail.com>
2024-10-01 00:23:10 +0530

    feat: user signing hook

--- src/hooks/useUserStore.ts
+++ src/hooks/useUserStore.ts
@@ -1,7 +1,5 @@
 import { User } from "firebase/auth";
 import { create } from "zustand";
-import { onAuthStateChanged } from "firebase/auth";
-import { firebaseAuth } from "../firebase/firebaseConfig"; // Your Firebase config
 
 type StoreType = {
   currentUser: User | null;
@@ -12,9 +10,3 @@ export const useUserStore = create<StoreType>((set) => ({
   currentUser: null,
   setCurrentUser: (user) => set({ currentUser: user }),
 }));
-
-// Listen for changes in authentication state when app starts
-onAuthStateChanged(firebaseAuth, (user) => {
-  const setCurrentUser = useUserStore.getState().setCurrentUser;
-  setCurrentUser(user); // Set the user in Zustand store
-});
